92a097e91bafcfc33e5eb2f86effa46cf201eda3
LauraPos <1214217@student.roc-nijmegen.nl>
2025-05-13 12:20:57 +0200

    DS7: Footer (Font veranderd)

--- NalaTravels/index.php
+++ NalaTravels/index.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nala Travels</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Imperial+Script&family=Julius+Sans+One&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
-<body>
+<body class="julius-sans-one-regular">
     <header>
         <?php
         include "assets/includes/header.inc.php";
@@ -21,7 +25,7 @@
 
     <footer>
         <?php
-        include "assets/includes/footer.inc.php"; 
+        include "assets/includes/footer.inc.php";
         ?>
     </footer>
 </body>
