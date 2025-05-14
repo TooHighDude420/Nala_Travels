c74b4a16550ccbf287823d1866d4a8d007e726a0
TooHighDude420 <toohighdudepruducts@gmail.com>
2025-05-14 10:44:52 +0200

    end of day

--- NalaTravels/assets/includes/home.inc.php
+++ NalaTravels/assets/includes/home.inc.php
@@ -2,40 +2,52 @@
     <div id="locationsHolder" class="pt-[20px] pl-[20px]">
         <h1>Uitgelichte Locaties</h1>
         <div id="cardHolder" class="flex gap-x-[5%] justify-center pl-[0px]">
-            <div id="leftCard" class="h-[10vh] w-[15%]">
-                <div id="leftHeader" class="bg-[#76C893] h-[5vh] rounded-tl-lg rounded-tr-lg flex flex-col content-center justify-center flex-wrap">
+            <div>
+                <img src="assets/img/cards/ArrowLeft.png" alt="LeftArrow">
+            </div>
+            
+            <div id="leftCard" class="h-[20vh] w-[15%]">
+                <div id="leftHeader"
+                    class="bg-[#76C893] h-[5vh] rounded-tl-md rounded-tr-md flex flex-col content-center justify-center flex-wrap">
                     <h1>Indonesie</h1>
                 </div>
                 <div id="leftContent">
                     <div class="w-full h-[20vh] bg-black">
-
+                        <img src="assets/img/cards/Indonesie.png" alt="Indonesie" class="h-full w-full">
                     </div>
                 </div>
             </div>
 
-            <div id="middleCard" class="h-[10vh] w-[20%]">
-                <div id="middleHeader" class="bg-[#52B69A] h-[5vh] rounded-tl-lg rounded-tr-lg flex flex-col content-center justify-center flex-wrap">
+            <div id="middleCard" class="h-[25vh] w-[15%]">
+                <div id="middleHeader"
+                    class="bg-[#52B69A] h-[5vh] rounded-tl-md rounded-tr-md flex flex-col content-center justify-center flex-wrap">
                     <h1>Servië</h1>
                 </div>
                 <div id="middleContent">
                     <div class="w-full h-[20vh] bg-black">
-
+                        <img src="assets/img/cards/Servie.png" alt="Servie" class="h-full w-full">
                     </div>
                 </div>
             </div>
 
 
 
-            <div id="rightCard" class="h-[10vh] w-[15%]">
-                <div id="rightHeader" class="bg-[#99D98C] h-[5vh] rounded-tl-lg rounded-tr-lg flex flex-col content-center justify-center flex-wrap">
+            <div id="rightCard" class="h-[25vh] w-[15%]">
+                <div id="rightHeader"
+                    class="bg-[#99D98C] h-[5vh] rounded-tl-md rounded-tr-md flex flex-col content-center justify-center flex-wrap">
                     <h1>Spanje</h1>
                 </div>
                 <div id="rightContent">
                     <div class="w-full h-[20vh] bg-black">
-
+                        <img src="assets/img/cards/Spanje.png" alt="Spanje" class="h-full w-full">
                     </div>
                 </div>
             </div>
+
+            <div>
+                <img src="assets/img/cards/ArrowRight.png" alt="RightArrow">
+            </div>
+
         </div>
     </div>
 </section>
