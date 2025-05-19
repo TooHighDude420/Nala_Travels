bd9dafb6375b985654da0ddbc91ce2d539b1f571
LauraPos <1214217@student.roc-nijmegen.nl>
2025-05-19 11:32:10 +0200

    RE5: Over ons pagina

--- NalaTravels/assets/includes/overons.inc.php
+++ NalaTravels/assets/includes/overons.inc.php
@@ -1,5 +1,45 @@
 <section>
-    <div>
-        
+    <div class="flex left h-[90vh] w-[100%] mt-10 justify-evenly">
+        <div class="bg-[#76C893] h-[80vh] w-[30%] rounded-[10px]">
+            <h1 class="ml-5 mt-3">
+                Over ons
+            </h1>
+            <p class="mt-5 mb-10 ml-5 mr-5">
+                Wij zijn Laura Pos en Nataro Ajaouin, studenten aan het ROC Technovium. Voor het vak Webapplicatie
+                hebben
+                wij
+                een reisbureau opgericht met een speciale focus op excursies. Wij geloven dat echte reiservaringen
+                beginnen
+                buiten de gebaande paden. Daarom organiseren we unieke en avontuurlijke excursies die jouw reis
+                onvergetelijk maken. Of je nu houdt van natuur, cultuur of actie – bij ons vind je de perfecte excursie.
+                Ontdek, beleef en geniet met ons reisbureau!
+            </p>
+            <h2 class="ml-5 mb-5">
+                Meer zien, meer beleven – excursies die je bijblijven.
+            </h2>
+        </div>
+        <div class="right  w-[30%]">
+            <div class="contact bg-[#52B69A] h-[20vh] rounded-[10px]">
+                <h1 class=" ml-5 pt-3">
+                    Contact
+                </h1>
+                <ul class="mt-5 ml-5 mr-5">
+                    <li>
+                        NalaTravels@travel.com
+                    </li>
+                    <li>
+                        +31 6 69696942
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h1>
+
+                </h1>
+                <p>
+
+                </p>
+            </div>
+        </div>
     </div>
 </section>
