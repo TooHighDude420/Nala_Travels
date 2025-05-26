a6bd7bbae7f0368c29a20b426aa87b980c8fab9f
TooHighDude420 <toohighdudepruducts@gmail.com>
2025-05-26 10:37:33 +0200

    RE3: carrousel werkt alleen database moet nog

--- NalaTravels/assets/includes/home.inc.php
+++ NalaTravels/assets/includes/home.inc.php
@@ -1,9 +1,9 @@
-<section>
+<section class="h-[80vh]">
     <div id="locationsHolder" class="pt-[20px] pl-[20px]">
         <h1>Uitgelichte Locaties</h1>
         <div id="cardHolder" class="flex gap-x-[5%] justify-center pl-[0px]">
             <div>
-                <img onclick="nextCard()" src="assets/img/cards/ArrowLeft.png" alt="LeftArrow">
+                <img onclick="test.nextCard()" src="assets/img/cards/ArrowLeft.png" alt="LeftArrow">
             </div>
 
             <div id="leftCardEnt" class="h-[20vh] w-[15%]">
@@ -45,12 +45,109 @@
             </div>
 
             <div>
-                <img onclick="prevCard()" src="assets/img/cards/ArrowRight.png" alt="RightArrow">
+                <img onclick="test.prevCard()" src="assets/img/cards/ArrowRight.png" alt="RightArrow">
             </div>
-
         </div>
     </div>
+
+    <div id="dealsHolder" class="pt-[20px] pl-[20px]">
+        <h1>Uitgelichte deals</h1>
+        <div id="dealCardHolder" class="flex gap-x-[5%] justify-center pl-[0px]">
+            <div>
+                <img onclick="testDeals.nextCard()" src="assets/img/cards/ArrowLeft.png" alt="LeftArrow">
+            </div>
+
+            <div id="dealLeftCardEnt" class="h-[20vh] w-[15%]">
+                <div id="dealLeftHeader"
+                    class="bg-[#76C893] h-[5vh] rounded-tl-md rounded-tr-md flex flex-col content-center justify-center flex-wrap">
+                    <h1 id="dealLeftTitle"></h1>
+                </div>
+                <div id="dealLeftContent">
+                    <div class="w-full h-[20vh] bg-black">
+                        <img id="dealLeftImage" src="" alt="" class="h-full w-full">
+                    </div>
+                </div>
+            </div>
+
+            <div id="dealCenterCardEnt" class="h-[25vh] w-[15%]">
+                <div id="middleHeader"
+                    class="bg-[#52B69A] h-[5vh] rounded-tl-md rounded-tr-md flex flex-col content-center justify-center flex-wrap">
+                    <h1 id="dealCenterTitle"></h1>
+                </div>
+                <div id="middleContent">
+                    <div class="w-full h-[20vh] bg-black">
+                        <img id="dealCenterImage" src="" alt="" class="h-full w-full">
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div id="dealRightCardEnt" class="h-[25vh] w-[15%]">
+                <div id="rightHeader"
+                    class="bg-[#99D98C] h-[5vh] rounded-tl-md rounded-tr-md flex flex-col content-center justify-center flex-wrap">
+                    <h1 id="dealRightTitle"></h1>
+                </div>
+                <div id="dealRightContent">
+                    <div class="w-full h-[20vh] bg-black">
+                        <img id="dealRightImage" src="" alt="" class="h-full w-full">
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <img onclick="testDeals.prevCard()" src="assets/img/cards/ArrowRight.png" alt="RightArrow">
+            </div>
+        </div>
+    </div>
+
+    <link rel="preload" href="./assets/img/cards/Indonesie.png" as="image">
+    <link rel="preload" href="./assets/img/cards/Servie.png" as="image">
+    <link rel="preload" href="./assets/img/cards/Spanje.png" as="image">
+
+
     <script src="assets/js/carrousel/Card.js"></script>
     <script src="assets/js/carrousel/Carrousel.js"></script>
     <script src="assets/js/carrousel/myWork.js"></script>
+    <script>
+        const test = new CarrMain(["leftCardEnt", "leftTitle", "leftImage"], ["centerCardEnt", "centerTitle", "centerImage"], ["rightCardEnt", "rightTitle", "rightImage"]);
+        test.go(
+            testArray = [
+                {
+                    Title: "Spanje",
+                    Img: "Spanje"
+                },
+
+                {
+                    Title: "Servië",
+                    Img: "Servie"
+                },
+
+                {
+                    Title: "Indonesië",
+                    Img: "Indonesie"
+                }
+            ]
+        );
+
+        const testDeals = new CarrMain(["dealLeftCardEnt", "dealLeftTitle", "dealLeftImage"], ["dealCenterCardEnt", "dealCenterTitle", "dealCenterImage"], ["dealRightCardEnt", "dealRightTitle", "dealRightImage"]);
+        testDeals.go(
+            testArray = [
+                {
+                    Title: "Spanje + Auto + Tour",
+                    Img: "Spanje"
+                },
+
+                {
+                    Title: "Servië + Hotel + Auto",
+                    Img: "Servie"
+                },
+
+                {
+                    Title: "Indonesië + Hotel",
+                    Img: "Indonesie"
+                }
+            ]
+        );
+    </script>
 </section>
--- NalaTravels/assets/js/carrousel/myWork.js
+++ NalaTravels/assets/js/carrousel/myWork.js
@@ -1,103 +1,95 @@
-var testArray = [
-    {
-        Title: "Spanje",
-        Img: "Spanje"
-    },
-    
-    {
-        Title: "Servië",
-        Img: "Servie"
-    },
-    
-    {
-        Title: "Indonesië",
-        Img: "Indonesie"
-    }
-];
-var carro;
-var xhttp;
+class CarrMain {
+    carro;
+    xhttp;
 
-// making the cards into objects
-const leftCardTotal = {
-    Ent: document.getElementById("leftCardEnt"),
-    Title: document.getElementById("leftTitle"),
-    Image: document.getElementById("leftImage")
-}
+    leftCardTotal;
+    centerCardTotal;
+    rightCardTotal;
 
-const centerCardTotal = {
-    Ent: document.getElementById("centerCardEnt"),
-    Title: document.getElementById("centerTitle"),
-    Image: document.getElementById("centerImage")
-}
+    constructor(leftIDS, centerIDS, rightIDS) {
+        this.leftCardTotal = {
+            Ent: document.getElementById(leftIDS[0]),
+            Title: document.getElementById(leftIDS[1]),
+            Image: document.getElementById(leftIDS[2])
+        }
 
-const rightCardTotal = {
-    Ent: document.getElementById("rightCardEnt"),
-    Title: document.getElementById("rightTitle"),
-    Image: document.getElementById("rightImage")
-}
+        this.centerCardTotal = {
+            Ent: document.getElementById(centerIDS[0]),
+            Title: document.getElementById(centerIDS[1]),
+            Image: document.getElementById(centerIDS[2])
+        }
 
-//herimplementeren als de database staat
-// xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//         testArray = this.response;
-//         testArray = JSON.parse(testArray);
-//         go();
-//     }
-// };
-
-// xhttp.open("GET", "../Port_upd/ajax/getProjects.php");
-// xhttp.send();
-
-// functie go van maken wanner de database staat
-//function go()
-carro = new Carroussel(testArray, 3);
-setCards()
-
-
-function nextCard() {
-    carro.nextCard();
-    setCards();
-}
-
-function prevCard() {
-    carro.prevCard();
-    setCards();
-}
-
-function setCards() {
-    leftCardTotal.Title.textContent = carro.getActiveCards()[0].title;
-    leftCardTotal.Image.src = carro.getActiveCards()[0].img;
-    leftCardTotal.Image.alt = carro.getActiveCards()[0].alt;
-    leftCardTotal.Ent.onclick = function () {
-        setModalContent([
-            carro.getActiveCards()[0].title,
-            carro.getActiveCards()[0].img,
-            carro.getActiveCards()[0].long_disc
-        ]);
+        this.rightCardTotal = {
+            Ent: document.getElementById(rightIDS[0]),
+            Title: document.getElementById(rightIDS[1]),
+            Image: document.getElementById(rightIDS[2])
+        }
     }
 
+    //herimplementeren als de database staat
+    // xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function () {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         testArray = this.response;
+    //         testArray = JSON.parse(testArray);
+    //         go();
+    //     }
+    // };
 
-    centerCardTotal.Title.textContent = carro.getActiveCards()[1].title;
-    centerCardTotal.Image.src = carro.getActiveCards()[1].img;
-    centerCardTotal.Image.alt = carro.getActiveCards()[1].alt;
-    centerCardTotal.Ent.onclick = function () {
-        setModalContent([
-            carro.getActiveCards()[1].title,
-            carro.getActiveCards()[1].img,
-            carro.getActiveCards()[1].long_disc
-        ]);
+    // xhttp.open("GET", "../Port_upd/ajax/getProjects.php");
+    // xhttp.send();
+
+    go(testArray) {
+        this.carro = new Carroussel(testArray, 3);
+        this.setCards();
     }
 
-    rightCardTotal.Title.textContent = carro.getActiveCards()[2].title;
-    rightCardTotal.Image.src = carro.getActiveCards()[2].img;
-    rightCardTotal.Image.alt = carro.getActiveCards()[2].alt;
-    rightCardTotal.Ent.onclick = function () {
-        setModalContent([
-            carro.getActiveCards()[2].title,
-            carro.getActiveCards()[2].img,
-            carro.getActiveCards()[2].long_disc
-        ]);
+    nextCard() {
+        this.carro.nextCard();
+        this.setCards();
+    }
+
+    prevCard() {
+        this.carro.prevCard();
+        this.setCards();
+    }
+
+    setCards() {
+        this.leftCardTotal.Title.textContent = this.carro.getActiveCards()[0].title;
+        this.leftCardTotal.Image.src = this.carro.getActiveCards()[0].img;
+        this.leftCardTotal.Image.alt = this.carro.getActiveCards()[0].alt;
+        this.leftCardTotal.Ent.onclick = function () {
+            setModalContent([
+                this.carro.getActiveCards()[0].title,
+                this.carro.getActiveCards()[0].img,
+                this.carro.getActiveCards()[0].long_disc
+            ]);
+        }
+
+
+        this.centerCardTotal.Title.textContent = this.carro.getActiveCards()[1].title;
+        this.centerCardTotal.Image.src = this.carro.getActiveCards()[1].img;
+        this.centerCardTotal.Image.alt = this.carro.getActiveCards()[1].alt;
+        this.centerCardTotal.Ent.onclick = function () {
+            setModalContent([
+                this.carro.getActiveCards()[1].title,
+                this.carro.getActiveCards()[1].img,
+                this.carro.getActiveCards()[1].long_disc
+            ]);
+        }
+
+        this.rightCardTotal.Title.textContent = this.carro.getActiveCards()[2].title;
+        this.rightCardTotal.Image.src = this.carro.getActiveCards()[2].img;
+        this.rightCardTotal.Image.alt = this.carro.getActiveCards()[2].alt;
+        this.rightCardTotal.Ent.onclick = function () {
+            setModalContent([
+                this.carro.getActiveCards()[2].title,
+                this.carro.getActiveCards()[2].img,
+                this.carro.getActiveCards()[2].long_disc
+            ]);
+        }
     }
 }
+
+
 
