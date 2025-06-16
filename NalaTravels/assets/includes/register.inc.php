b9cb6dd9a7ef457f56e4490217d5d204ae09978e
TooHighDude420 <toohighdudepruducts@gmail.com>
2025-06-16 11:03:34 +0200

    Laura

--- NalaTravels/assets/includes/register.inc.php
+++ NalaTravels/assets/includes/register.inc.php
@@ -5,21 +5,23 @@
         </a>
     </div>
 
-    <div class="bg-[#34A0A4] border-solid border-2 border-black rounded-[50px] h-[60vh] w-[50%] opacity-80">
+    <div class="bg-col-login bg-[#34A0A4] border-solid border-2 border-black rounded-[50px] h-[50vh] w-[35%] opacity-80">
         <form action="assets/php/register.php" method="post"
-            class="h-full w-full flex flex-col justify-around items-center">
-            <div>
-                <label class="text-white" for="username">Username</label>
-                <input type="text" name="username" placeholder="Username">
+            class="h-[40vh] w-full flex flex-col justify-around items-center mt-10">
+            <div class="">
+                <input type="text" name="username" placeholder=" Username" class="rounded-[5px] h-[4vh] mr-3">
+                <input type="text" name="email" placeholder=" Email" class="rounded-[5px] h-[4vh]">
             </div>
             <div>
-                <label for="password">Password</label>
-                <input type="password" name="password" placeholder="Password">
-                <label for="passwordRep">Repeat Password</label>
-                <input type="password" name="passwordRep" placeholder="Password">
+                <input type="password" name="password" placeholder=" Password" class="rounded-[5px] h-[4vh] mr-3">
+                <input type="password" name="passwordRep" placeholder=" Repeat password" class="rounded-[5px] h-[4vh]">
             </div>
             <div>
-                <button type="submit">Register!</button>
+                <input type="text" name="fName" placeholder=" First name" class="rounded-[5px] h-[4vh] mr-3">
+                <input type="text" name="lName" placeholder=" Last name" class="rounded-[5px] h-[4vh]">
+            </div>
+            <div>
+                <button type="submit" class="text-white bg-black w-[13vh] h-[5vh] rounded-[5px] text-center content-center">Register!</button>
             </div>
         </form>
     </div>
