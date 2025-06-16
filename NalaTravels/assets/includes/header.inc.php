098c7915582bc9b76ab938a39a23f31420ad79e7
LauraPos <1214217@student.roc-nijmegen.nl>
2025-06-16 12:52:37 +0200

    eod

--- NalaTravels/assets/includes/header.inc.php
+++ NalaTravels/assets/includes/header.inc.php
@@ -79,12 +79,12 @@
                             </div>
                         </div>
                         <form action="assets/php/search.php" method="post"
-                            class="onder bg-[#168AAD] h-[7vh] rounded-[3px] flex justify-evenly content-center flex-wrap">
+                            class="onder bg-[#168AAD] h-[7vh] flex justify-evenly content-center flex-wrap">
                             <input type="text" name="departure" id="departure"
-                                class="inputFields placeholder:text-center" placeholder="Vertrek">
-                            <input type="text" name="arrival" id="arrival" class="inputFields placeholder:text-center"
+                                class="inputFields placeholder:text-center rounded-[5px]" placeholder="Vertrek">
+                            <input type="text" name="arrival" id="arrival" class="inputFields placeholder:text-center rounded-[5px]"
                                 placeholder="Bestemming">
-                            <select name="groupSize" id="groupSize" class="inputFields">
+                            <select name="groupSize" id="groupSize" class="inputFields rounded-[5px]">
                                 <option value="def" selected="true" disabled="disabled" class="text-gray-500">Aantal
                                     personen
                                 </option>
@@ -103,7 +103,7 @@
                                     </svg>
                                 </div>
                                 <input datepicker id="default-datepicker" type="text"
-                                    class="h-[5vh] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    class="h-[5vh] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
                                     placeholder="Select date">
                             </div>
                             <button type="submit">
