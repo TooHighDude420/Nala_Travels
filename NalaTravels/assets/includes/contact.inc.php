11ffcc4ce8e2db43c1c1745b0d9bab5cde827920
LauraPos <1214217@student.roc-nijmegen.nl>
2025-06-18 10:31:55 +0200

    contact formulier

---
+++ NalaTravels/assets/includes/contact.inc.php
@@ -0,0 +1,34 @@
+<section class="login h-fit bg-white dark:bg-gray-900">
+    <div class="px-4 mx-auto max-w-screen-md pb-[30px]">
+        <h2 class="mb-3 pt-[20px] text-4xl tracking-tight font-extrabold text-center text-[#B5E48C] ">Contact Us
+        </h2>
+        <p class="mb-10 font-light text-center text-gray-500">Got a technical
+            issue? Have any questions? Let us know.</p>
+        <form action="#" class="space-y-5">
+            <div>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your
+                    email</label>
+                <input type="email" id="email"
+                    class="bg-[#99D98C] placeholder-white text-sm rounded-[10px] block w-full p-2.5"
+                    placeholder="NalaTravels@travel.com" required>
+            </div>
+            <div>
+                <label for="subject"
+                    class="block mb-2 text-sm font-medium text-gray-900">Subject</label>
+                <input type="text" id="subject"
+                    class="bg-[#76CB93] placeholder-white text-sm rounded-[10px] block w-full p-2.5"
+                    placeholder="Let us know how we can help you" required>
+            </div>
+            <div class="sm:col-span-2">
+                <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Your
+                    message</label>
+                <textarea id="message" rows="6"
+                    class="bg-[#52B69A] placeholder-white text-sm rounded-[10px] block w-full p-2.5"
+                    placeholder="Leave a comment..."></textarea>
+            </div>
+            <button type="submit"
+                class="text-white rounded-[5px] text-white bg-black w-[30vh] h-[5vh]">Send
+                message</button>
+        </form>
+    </div>
+</section>
